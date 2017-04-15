5601081afc39f814fb1f28c2835f7320f2369885
Pham Dinh Vu <vuphamas@gmail.com>
2017-04-15 19:24:51 +0000

    Signin/Signup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,20 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.atwho
 //= require jquery_ujs
-//= require turbolinks
+//= require bootstrap
+//= require bindWithDelay
+//= require jquery.datetimepicker
 //= require_tree .
+
+$(document).ready(function(){
+  $("#event_when").datetimepicker({
+    format:'Y/m/d H:i'
+  });
+  $("#user_dob").datetimepicker({
+    timepicker:false,
+    format:'Y/m/d',
+    maxDate:'0'
+  });
+});
